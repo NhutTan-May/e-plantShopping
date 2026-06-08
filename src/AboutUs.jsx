89d9791e8103e0d628955ede93f60d0119b0dd9e
Tanln2203 <lenhuttan321@gmail.com>
2026-06-08 10:19:26 +0700

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div className="about-us-container">
+      <h2>About Paradise Nursery</h2>
+      <p>
+        At Paradise Nursery, we are passionate about greenery. Founded with the belief that plants make people happier and spaces healthier, we curate a diverse collection of indoor and outdoor plants. Whether you are a seasoned botanist or a curious beginner, our team is here to help you cultivate your perfect green sanctuary.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
